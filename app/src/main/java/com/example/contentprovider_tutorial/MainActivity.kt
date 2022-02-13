fc3b6f00040157e3e44da6be50aa93ab00e99d6a
ParkJong-Hun <thesn515@gmail.com>
2022-02-13 16:27:44 +0900

    2022/02/13 16:27 get contacts

--- app/src/main/java/com/example/contentprovider_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/contentprovider_tutorial/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.example.contentprovider_tutorial
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            getPhoneNumbers()
+        }
+    }
+
+    //연락처를 가져오기
+    private fun getPhoneNumbers() {
+        //권한 체크 후 없으면 요청
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_CONTACTS), 0)
+        }
+        //로깅할 결과값
+        var result = ""
+        //연락처 앱에 보낼 URI
+        val uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+        //ContentResolver를 이용해 query로 URI를 요청하여 얻은 Cursor
+        val cursor = contentResolver.query(
+            uri,
+            null,
+            null,
+            null,
+            null
+        )
+        cursor?.let {
+            if(it.count > 0) {
+                it.moveToFirst()
+                do {
+                    //이름 부분 반환
+                    val name = it.getString(it.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                    //번호 부분 반환
+                    val phoneNumber = it.getString(it.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    result += "이름: $name 폰번호: $phoneNumber\n"
+                } while (cursor.moveToNext())
+            } else {
+                result += "No data."
+            }
+            it.close()
+        }
+        Log.d("result", result)
     }
 }
 
@@ -19,6 +66,12 @@ ContentResolver
 각 앱의 ContentProvider를 중계하는 역할을 한다.
 ContentResolver가 URI를 요청하면, 그에 맞게 ContentProvider는 Database에서 데이터를 찾아서 ContentResolver를 통해 다른 앱에 전달한다.
 ContentResolver는 Query로 ContentProvider를 이용해 데이터 CRUD가 가능하다.
+ContentResolver : query(uri, projection, selection, selectionArgs, sortOrder)
+    uri : content://scheme 방식의 원하는 데이터를 가져오기 위한 정해진 주소
+    projection : 가져올 컬럼 이름 목록, null이면 모든 컬럼
+    selection : where 절에 해당하는 내용
+    selectionArgs : selection에서 ?로 표시한 곳에 들어갈 데이터
+    sortOrder : 정렬을 위한 order by 구문
 
 android.Provider
 연락처, 전화기록, 오디오, 비디오 등 네이티브 앱과의 소통을 하는 ContentProvider이다.
